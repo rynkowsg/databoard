f3013e4d25f99c0997e3f620bbcfd9b3fcedf2cc
Greg Rynkowski <5878299+rynkowsg@users.noreply.github.com>
2023-03-24 11:38:22 +0000

    Fix blinking of loading translations

--- src/main/databoard/client/components/root.cljs
+++ src/main/databoard/client/components/root.cljs
@@ -12,16 +12,27 @@
   (comp/get-initial-state i18n/Locale {:locale dcu-language/init-locale}))
 #_(get-current-locale-initial-state)
 
-(comp/defsc Root [_this {:root/keys [login] ::i18n/keys [current-locale] :as props}]
-  {:query [[df/marker-table :translations-loading]
+(def initial-transl-marker [df/marker-table :initial-transl])
+
+(comp/defsc Root [this {:root/keys [login] ::i18n/keys [current-locale] :as props}]
+  {:query [initial-transl-marker
            {:root/login (comp/get-query dcc-login/Login)}
            {::i18n/current-locale (comp/get-query i18n/Locale)}]
    :initial-state (fn [_] {:root/login (comp/get-initial-state dcc-login/Login {})
                            ::i18n/current-locale (get-current-locale-initial-state)})
-   :componentWillMount (fn [this] (dcu-language/load-translations-with-marker this :translations-loading))}
-  (let [translations-loading? (df/loading? (get props [df/marker-table :translations-loading]))
-        _ (js/console.log "[ui] render Root, translations-loading?:" translations-loading? current-locale)]
-    (when-not translations-loading?
+   :UNSAFE_componentWillMount (fn [this] (dcu-language/load-translations-with-marker this :initial-transl))
+   :initLocalState (fn [_ _] {:transl-on-start (atom :none)})} ;; :none :loading :ready
+  (let [transl-loading? (df/loading? (get props initial-transl-marker))
+        transl-ready? (let [initial-transl (comp/get-state this :transl-on-start)]
+                        (swap! initial-transl #(cond
+                                                 (and (= % :none) (not transl-loading?)) :none
+                                                 (and (= % :none) transl-loading?) :loading
+                                                 (and (= % :loading) transl-loading?) :loading
+                                                 (and (= % :loading) (not transl-loading?)) :ready
+                                                 :ready :ready))
+                        @initial-transl)
+        _ (js/console.log "[ui] render Root, transl-ready?" transl-ready? "transl-loading?:" transl-loading? (:ui/locale-name current-locale) (::i18n/translations current-locale))]
+    (when (= transl-ready? :ready)
       (dom/div
        (dom/h1 (tr "Databoard"))
        (dom/h4 (tr "My data & charts"))
